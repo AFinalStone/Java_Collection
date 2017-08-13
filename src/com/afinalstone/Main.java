46af67f1b4d2c1e3ed7878b77196c0c593186a72
AFinalStone <afinalstone@foxmail.com>
2017-08-13 18:40:29 +0800

    添加Map集合介绍

--- src/com/afinalstone/Main.java
+++ src/com/afinalstone/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     static String [] s = new String[]{"1","2","3"};
     public static void main(String[] args) {
-        System.out.println("Hello World");
         System.out.println("数组结果"+s);
-        System.out.println("数组结果");
     }
 }
